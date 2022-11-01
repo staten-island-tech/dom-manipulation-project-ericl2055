b61832b7695544484c60bfaa9e82aecf6114e5c5
ericl2055 <eric_lin2055@icloud.com>
2022-11-01 14:00:22 -0400

    dom

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const DOMselectors = {};
+const DOMselectors = {
+  button: document.getElementById("button"),
+  text: document.querySelector("both"),
+};
+console.log(DOMselectors.button);
+function text(text) {
+  text.textContent = "song name, artist name";
+  text.style.fontSize = "20px";
+}
+DOMselectors.button.addEventListener("click", function () {
+  text(DOMselectors.text);
+});
