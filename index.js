52d43088995365fbe74142532d5bf24d7c0d905d
ericl2055 <eric_lin2055@icloud.com>
2022-11-04 13:58:51 -0400

    dom

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const DOMSelectors = {
   button: document.getElementById("btn"),
+  reset: document.getElementById("btn2"),
   text: document.getElementById("text"),
   text2: document.getElementById("text2"),
+  a: document.querySelector("img"),
   input: document.querySelector("#songname"),
   input2: document.querySelector("#artistname"),
+  input3: document.querySelector("#imglink"),
 };
-
+function image() {
+  let input3 = DOMSelectors.input3.value;
+  DOMSelectors.a.src = `${input3}`;
+  DOMSelectors.input3.value = "";
+}
+DOMSelectors.reset.addEventListener("click", function () {
+  DOMSelectors.text.value = "";
+  DOMSelectors.text2.value = "";
+  DOMSelectors.a.src = "";
+});
 DOMSelectors.button.addEventListener("click", function () {
   let input = DOMSelectors.input.value;
   let input2 = DOMSelectors.input2.value;
@@ -13,4 +25,5 @@ DOMSelectors.button.addEventListener("click", function () {
   DOMSelectors.input.value = "";
   DOMSelectors.text2.insertAdjacentHTML("afterend", `<p>${input2}</p>`);
   DOMSelectors.input2.value = "";
+  image(DOMSelectors.a);
 });
