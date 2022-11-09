c3e80d418f8f2e0cdfbee980cd6786f2d3f8e58a
ericl2055 <eric_lin2055@icloud.com>
2022-11-09 13:59:38 -0500

    dom

--- index.js
+++ index.js
@@ -1,34 +1,24 @@
 const DOMSelectors = {
   button: document.getElementById("btn"),
-  text: document.getElementById("text"),
-  text2: document.getElementById("text2"),
-  a: document.querySelector("img"),
+  box: document.getElementById("parent"),
   input: document.querySelector("#songname"),
   input2: document.querySelector("#artistname"),
   input3: document.querySelector("#imglink"),
+  a: document.querySelector("img"),
 };
-function makeCard() {
+
+function removeParent(d) {
+  d.remove();
+}
+DOMSelectors.button.addEventListener("click", function () {
   let input = DOMSelectors.input.value;
   let input2 = DOMSelectors.input2.value;
   let input3 = DOMSelectors.input3.value;
-  DOMSelectors.text.insertAdjacentHTML("afterend", `<p>${input}</p>`);
+  DOMSelectors.box.insertAdjacentHTML(
+    "beforeend",
+    `<p> - ${input} ${input2} <img src  = ${input3}> <button onclick="removeParent(this.parentNode)">Finished</button </p>`
+  );
   DOMSelectors.input.value = "";
-  DOMSelectors.text2.insertAdjacentHTML("afterend", `<p>${input2}</p>`);
   DOMSelectors.input2.value = "";
-  DOMSelectors.a.src = `${input3}`;
   DOMSelectors.input3.value = "";
-  const delbutton = document.createElement("button");
-  document.querySelectorAll("#delbutton");
-  DOMSelectors.a.appendChild(delbutton);
-  delbutton.forEach((button) => {
-    button.addEventListener("click", function () {
-      DOMSelectors.text.value = "";
-      DOMSelectors.text2.value = "";
-      DOMSelectors.a.src = "";
-    });
-  });
-}
-
-DOMSelectors.button.addEventListener("click", function () {
-  makeCard(DOMSelectors);
 });
