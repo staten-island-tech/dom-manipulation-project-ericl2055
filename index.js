9d09ec510daf2aa876ea3f31e49c0e30c8452a21
ericl2055 <eric_lin2055@icloud.com>
2022-11-10 13:56:28 -0500

    DOM

--- index.js
+++ index.js
@@ -7,18 +7,35 @@ const DOMSelectors = {
   a: document.querySelector("img"),
 };
 
-function removeParent(d) {
-  d.remove();
+function removeParent(a) {
+  a.remove();
 }
 DOMSelectors.button.addEventListener("click", function () {
   let input = DOMSelectors.input.value;
   let input2 = DOMSelectors.input2.value;
   let input3 = DOMSelectors.input3.value;
-  DOMSelectors.box.insertAdjacentHTML(
-    "beforeend",
-    `<p> - ${input} ${input2} <img src  = ${input3}> <button onclick="removeParent(this.parentNode)">Finished</button </p>`
-  );
-  DOMSelectors.input.value = "";
-  DOMSelectors.input2.value = "";
-  DOMSelectors.input3.value = "";
+  if (input === "") {
+    DOMSelectors.box.insertAdjacentHTML(
+      "beforeend",
+      `<p> No Movie Title  -  <button onclick="removeParent(this.parentNode)">Done</button</p>`
+    );
+  } else if (input2 === "") {
+    DOMSelectors.box.insertAdjacentHTML(
+      "beforeend",
+      `<p> No Cast  -  <button onclick="removeParent(this.parentNode)">Done</button</p>`
+    );
+  } else if (input3 === "") {
+    DOMSelectors.box.insertAdjacentHTML(
+      "beforeend",
+      `<p> No Image  -  <button onclick="removeParent(this.parentNode)">Done</button</p>`
+    );
+  } else {
+    DOMSelectors.box.insertAdjacentHTML(
+      "beforeend",
+      `<p>  ${input} ${input2} <img src  = ${input3}>  -  <button onclick="removeParent(this.parentNode)">Finished</button </p>`
+    );
+    DOMSelectors.input.value = "";
+    DOMSelectors.input2.value = "";
+    DOMSelectors.input3.value = "";
+  }
 });
